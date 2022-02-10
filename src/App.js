56def1a9e3c7e1ad81240c8e5fa5523e8bb7ad3c
Jeppe Bentzen <jeb.bentzen@gmail.com>
2022-02-10 10:06:51 +0100

    krav

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <p>Billede</p>
+      <p>Navn</p>
+      <p>Titel</p>
+      <p>Mail</p>
+      <p>Mail btn</p>
+      <p>Linkdin btn</p>
+      <p>About</p>
+      <p>About text</p>
+      <p>Intrests</p>
+      <p>Intrests text</p>
+    </div>
   );
 }
 
