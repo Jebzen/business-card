c8d110671ad0242fc911f84382665cb8c808e2fe
jebzen <jeb.bentzen@gmail.com>
2022-02-17 10:01:38 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Picture from './components/picture.js';
 import Body from './components/body.js'
+import footer from './components/footer.js'
 
 function App() {
   return (
     <div className='card'>
         {Picture}
         {Body}
+        {footer}
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,32 @@
+import './footer.css'
+
+const footer = 
+    <div className='footer'>
+        <div>
+            <a href='https://twitter.com/Jebzen_tweet' target="_blank">
+                <i class="bi bi-twitter"></i>
+            </a>
+        </div>
+        <div>
+            <a href='https://www.facebook.com/jeb.bentzen/' target="_blank">
+                <i class="bi bi-facebook"></i>
+            </a>
+        </div>
+        <div>
+            <a href='#' target="_blank">
+                <i class="bi bi-instagram"></i>
+            </a>
+        </div>
+        <div>
+            <a href='https://www.linkedin.com/in/jeppe-bentzen-b291ba1b8/' target="_blank">
+                <i class="bi bi-linkedin"></i>
+            </a>
+        </div>
+        <div>
+            <a href='https://github.com/Jebzen' target="_blank">
+                <i class="bi bi-github"></i>
+            </a>
+        </div>
+    </div>
+
+export default footer;
