c918a82c32dbe4c53f379c9beb6daccd47f7c596
jebzen <jeb.bentzen@gmail.com>
2022-02-17 10:06:09 +0100

    Icons in buttons

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -2,14 +2,14 @@ import './buttons.css'
 
 const Buttons = 
     <div className='buttons'>
-        <button
-            className='email'>
+        <a className='email' href='#' target="_blank">
             Email
-        </button>
-        <button 
-            className='linkdin'>
+            <i class="bi bi-envelope"></i>
+        </a>
+        <a className='linkdin' href='https://www.linkedin.com/in/jeppe-bentzen-b291ba1b8/' target="_blank">
             Linkedin
-        </button>
+            <i class="bi bi-linkedin"></i>
+        </a>
     </div>
 
 export default Buttons;
